004143aaefb6bab1530b63b147c3c0615f1ba658
g3rfo <rozhchenkoivan@gmail.com>
2023-12-17 23:00:43 +0200

    Add project files.

---
+++ Lab14.2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <stdio.h>
+#define size 10
+
+int main()
+{
+	int i, *pointer;
+	float average = 0;
+
+	int array[size];
+
+	for (i = 0; i < size; i++) {
+		printf("Enter %d number: ", i + 1);
+		scanf_s("%d", &array[i]);
+		pointer = &array[i];
+		average += *pointer;
+	}
+	
+	printf("Average is: %.2f", average / size);
+}
